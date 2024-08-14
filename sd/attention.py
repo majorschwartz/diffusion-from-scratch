2c661187d9261e7ac8b1b853cf630e6823edf4f6
Major <107882188+qubitflip@users.noreply.github.com>
2024-08-14 12:10:17 -0400

    Pipeline and DDPM

--- sd/attention.py
+++ sd/attention.py
@@ -51,4 +51,43 @@ class SelfAttention(nn.Module):
 		# (batch_size, seq_len, dim)
 		return output
 
+class CrossAttention(nn.Module):
+	def __init__(self, n_heads: int, d_embed: int, d_cross: int, in_proj_bias=True, out_proj_bias=True):
+		super().__init__()
 		
+		self.q_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+		self.k_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+		self.v_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+		self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+		self.n_heads = n_heads
+		self.d_head = d_embed // n_heads
+	
+	def forward(self, x, y):
+		# x: (latent): (batch_size, seq_len_q, dim_q)
+		# y: (context): (batch_size, seq_len_kv, dim_kv)
+		
+		input_shape = x.shape
+		batch_size, seq_len, d_embed = input_shape
+		intermed_shape = (batch_size, -1, self.n_heads, self.d_head)
+
+		# Multiply query by Wq
+		q = self.q_proj(x)
+		# Multiply keys by Wk
+		k = self.k_proj(y)
+		# Multiply values by Wv
+		v = self.v_proj(y)
+
+		q = q.view(intermed_shape).transpose(1, 2)
+		k = k.view(intermed_shape).transpose(1, 2)
+		v = v.view(intermed_shape).transpose(1, 2)
+
+		weight = q @ k.transpose(-1, -2)
+		weight /= math.sqrt(self.d_head)
+		weight = F.softmax(weight, dim=-1)
+
+		output = weight @ v
+		output = output.transpose(1, 2).contiguous()
+		output = output.view(input_shape)
+		output = self.out_proj(output)
+
+		return output
